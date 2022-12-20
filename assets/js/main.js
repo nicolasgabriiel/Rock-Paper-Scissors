d4e9a84a5335860cb576847563997a2f03f4a613
Nicolas <nicolasgabriel.developer@gmail.com>
2022-12-19 22:24:56 -0300

    Gerando o oponente aleatoriamente
    gerando o oponente aleatóriamente usando a função math

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,22 +6,30 @@ let youPicked = document.querySelector('.you-picked')
 
 let housePicked = document.querySelector('.house-picked')
 
+let codeHouse;
+
 let codigo;
+
+let codigoOponente;
+
 function seletor(code){
     codigo = code
     fecharPrincipal()
     mostrarIndividual()
     selectionOne()
-    selectionTwo()
-   
+    selectionTwo()  
+    gerarAleatório()
+    setTimeout(gerarOponente, 3000)
 }
 
 function fecharPrincipal (){
     containerBotoes.classList.add('hide')
 }
+
 function mostrarIndividual (){
 containerDisputa.classList.remove('hide')
 }
+
 function selectionOne(){
     youPicked.classList.add('button')
     youPicked.classList.add('button-individual')
@@ -41,9 +49,39 @@ function selectionOne(){
         youPicked.classList.add('button4')
     }
 }
+
 function selectionTwo() {
     housePicked.classList.add('button')
     housePicked.classList.add('button-individual')
     housePicked.classList.add('house-Vazia')
+}
 
+function gerarAleatório (min, max) {
+    min = Math.ceil(1);
+    max = Math.floor(5);
+    codeHouse = Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function gerarOponente(){
+    if(codeHouse == 1){
+        housePicked.classList.remove('house-Vazia')
+        housePicked.classList.add('button1')
+        codigoOponente = 'tesoura'
+    }else if(codeHouse == 2){
+        housePicked.classList.remove('house-Vazia')
+        housePicked.classList.add('button2')
+        codigoOponente = 'papel'
+    }else if(codeHouse == 3){
+        housePicked.classList.remove('house-Vazia')
+        housePicked.classList.add('button3')
+        codigoOponente = 'spock'
+    }else if(codeHouse == 4){
+        housePicked.classList.remove('house-Vazia')
+        housePicked.classList.add('button4')
+        codigoOponente = 'lagarto'
+    }else {
+        housePicked.classList.remove('house-Vazia')
+        housePicked.classList.add('button5')
+        codigoOponente = 'pedra'
+    }
 }
